3d6fb7530dc0816e351a7b063784eed0627cb0c1
zinmyomaung <zinmyomaung@comquas.com>
2021-08-15 20:48:55 +0630

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <router-link class="nav-link" :to="{ name: 'home' }">HOME</router-link> 
+            </li>
+            <li class="nav-item active">
+              <router-link class="nav-link" :to="{ name: 'news' }">NEWS</router-link> 
+            </li>
+            <li class="nav-item active">
+              <router-link class="nav-link" :to="{ name: 'matches' }">MATCHES</router-link>  
+            </li>
+            <li class="nav-item active">
+              <router-link class="nav-link" :to="{ name: 'teams' }">TEAMS</router-link> 
+            </li>
+            <li class="nav-item active">
+              <router-link class="nav-link" :to="{ name: 'tickets' }">TICKETS</router-link>             
+            </li>
+            <li class="nav-item active">
+              <router-link class="nav-link" :to="{ name: 'clubs' }">CLUBS</router-link> 
+            </li>
+            <li class="nav-item active">
+              <router-link class="nav-link" :to="{ name: 'shops' }">SHOPS</router-link>  
+            </li>
+          </ul>
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <router-link class="nav-link" to="">LOG IN</router-link>             
+            </li>
+            <li class="nav-item active">
+              <router-link class="nav-link" to="">SIGN UP</router-link> 
+            </li>
+          </ul>
+          <!-- <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form> -->
+        </div>
+      </nav>
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+@import'~bootstrap/dist/css/bootstrap.css'
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,50 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import News from "../views/News.vue";
+import Match from "../views/Match.vue";
+import Team from "../views/Team.vue";
+import Ticket from "../views/Ticket.vue";
+import Club from "../views/Club.vue";
+import Shop from "../views/Shop.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/news",
+    name: "news",
+    component: News,
+  },
+  {
+    path: "/matches",
+    name: "matches",
+    component: Match,
+  },
+  {
+    path: "/teams",
+    name: "teams",
+    component: Team,
+  },
+  {
+    path: "/tickets",
+    name: "tickets",
+    component: Ticket,
+  },
+  {
+    path: "/clubs",
+    name: "clubs",
+    component: Club,
+  },
+  {
+    path: "/shops",
+    name: "shops",
+    component: Shop,
   },
 ];
 
